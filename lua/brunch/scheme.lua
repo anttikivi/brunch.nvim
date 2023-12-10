6389bc4d840f6cdf67bb9571a63560705dc7596a
Antti Kivi <antti.kivi@visiosto.fi>
2023-12-10 15:48:25 +0200

    feat: add support for semantic LSP tokens

--- lua/brunch/scheme.lua
+++ lua/brunch/scheme.lua
@@ -33,6 +33,8 @@ function M.create(opts)
   ---@field ctermfg string|number|nil
   ---@field ctermbg string|number|nil
 
+  -- The default highlight groups and their values:
+  -- https://github.com/neovim/neovim/blob/master/src/nvim/highlight_group.c
   ---@alias Highlights table<string, Highlight>
   ---@type Highlights
   scheme.highlights = {
@@ -489,6 +491,38 @@ function M.create(opts)
     -- Misc
     gitcommitSummary = { fg = c.rosewater, style = { 'italic' } },
     zshKSHFunction = { link = 'Function' },
+
+    -- LSP Semantic Token Groups
+    ['@lsp.type.boolean'] = { link = '@boolean' },
+    ['@lsp.type.builtinType'] = { link = '@type.builtin' },
+    ['@lsp.type.comment'] = { link = '@comment' },
+    ['@lsp.type.enum'] = { link = '@type' },
+    ['@lsp.type.enumMember'] = { link = '@constant' },
+    ['@lsp.type.escapeSequence'] = { link = '@string.escape' },
+    ['@lsp.type.formatSpecifier'] = { link = '@punctuation.special' },
+    ['@lsp.type.interface'] = { fg = c.grapefruit },
+    ['@lsp.type.keyword'] = { link = '@keyword' },
+    ['@lsp.type.namespace'] = { link = '@namespace' },
+    ['@lsp.type.number'] = { link = '@number' },
+    ['@lsp.type.operator'] = { link = '@operator' },
+    ['@lsp.type.parameter'] = { link = '@parameter' },
+    ['@lsp.type.property'] = { link = '@property' },
+    ['@lsp.type.selfKeyword'] = { link = '@variable.builtin' },
+    ['@lsp.type.typeAlias'] = { link = '@type.definition' },
+    ['@lsp.type.unresolvedReference'] = { link = '@error' },
+    ['@lsp.type.variable'] = {}, -- use treesitter styles for regular variables
+    ['@lsp.typemod.class.defaultLibrary'] = { link = '@type.builtin' },
+    ['@lsp.typemod.enum.defaultLibrary'] = { link = '@type.builtin' },
+    ['@lsp.typemod.enumMember.defaultLibrary'] = { link = '@constant.builtin' },
+    ['@lsp.typemod.function.defaultLibrary'] = { link = '@function.builtin' },
+    ['@lsp.typemod.keyword.async'] = { link = '@keyword.coroutine' },
+    ['@lsp.typemod.macro.defaultLibrary'] = { link = '@function.builtin' },
+    ['@lsp.typemod.method.defaultLibrary'] = { link = '@function.builtin' },
+    ['@lsp.typemod.operator.injected'] = { link = '@operator' },
+    ['@lsp.typemod.string.injected'] = { link = '@string' },
+    ['@lsp.typemod.type.defaultLibrary'] = { link = '@type.builtin' },
+    ['@lsp.typemod.variable.defaultLibrary'] = { link = '@variable.builtin' },
+    ['@lsp.typemod.variable.injected'] = { link = '@variable' },
 
     -- Illuminate
     illuminatedWord = { bg = c.surface1 },
